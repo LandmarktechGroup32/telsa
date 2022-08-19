776e0484469a04ea9d7b27b426726dfdaedac738
OluwamayowaYewande <omoladeedee1@gmail.com>
2022-08-19 23:12:25 +0000

    dev env deployment

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-#!/bin/bash
-echo "Please enter the fee amount that you can pay for the course"
-read fee
-if [ $fee == 3500 ]
-then
-echo "Congratulations $name, your price is accepted"
-echo "Welcome to LandmarkTechnolgy"
-echo "You're rich"
-fi
+deploy java applications
+deploy python apps
+deploy in the dev env 
