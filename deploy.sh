097c58adf570bb8dd59598ff3993585394de80c6
OluwamayowaYewande <omoladeedee1@gmail.com>
2022-08-20 17:15:04 +0000

    conflict 2 resolved conflict reolved 3

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,16 @@ Deploy python apps
 deploy in the dev env 
 deployment5 master 
 deployment4
+<<<<<<< HEAD
+=======
+HEAD
+
+>>>>>>> 45b2ec9 (conflict 2 resolved)
 deploy to staging
 echo good jon
 tested and accepted
 
+<<<<<<< HEAD
+=======
+1839536 (tested abd approved by the client QA team)
+>>>>>>> 45b2ec9 (conflict 2 resolved)
